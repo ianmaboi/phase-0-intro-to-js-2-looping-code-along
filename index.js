1c9ff1463bd335529cd5565b3fb0cd4dc529cbd9
ianmaboimonene <ian.maboi@student.moringaschool.com>
2023-09-24 13:34:21 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards( names, event){
+    const ThankYouMessages =[];
+    for(let i=0;i<names.length;i++){
+        const message =`Thank you,${names[i]},for the wonderful ${event} gift!`;
+        ThankYouMessages.push(message);
+    }
+    return ThankYouMessages;
+}
+
+const names =["Guadalupe","Ollie","Aki"];
+const event ="birthday";
+const messages =writeCards(names,Event);
+console.log(messages);
+
+function countdown(number){
+    let i =number;
+    while (i>=0){
+        console.log(i);
+        i--;
+    }
+}
+    countdown(10);
